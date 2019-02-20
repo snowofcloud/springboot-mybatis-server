7a16ea497a70d202a414e5235e25c72c6bab56ef
xxq <xuxq1015@163.com>
2019-02-20 23:25:20 +0800

    validation后台实现参数校验

--- src/main/java/com/q8888/springboot/mybatis/server/controller/UserController.java
+++ src/main/java/com/q8888/springboot/mybatis/server/controller/UserController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,7 @@ public class UserController {
     //RequestMapping方式
     @ResponseBody
     @RequestMapping(value = "/add", produces = {"application/json;charset=UTF-8"})
-    public JsonResult addUser(User user){
+    public JsonResult addUser(@Validated User user){
         return new JsonResult(JsonResult.State.SUCCESS,userService.addUser(user));
     }
 
